8b6d15ac7d6c89f1dd2e6e950fb436e1ef651eab
KananValizada <kenan.veli@yahoo.com>
2022-11-10 22:17:08 +0400

    sign up page

--- app/src/main/java/com/example/wordgame/SignUpActivity.kt
+++ app/src/main/java/com/example/wordgame/SignUpActivity.kt
@@ -1,19 +1,19 @@
 package com.example.wordgame
 
-import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
-import com.example.wordgame.databinding.ActivityIntroBinding
+import com.example.wordgame.databinding.ActivitySignUpBinding
 
-class IntroActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityIntroBinding
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityIntroBinding.inflate(layoutInflater)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupActionBar()
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
             window.insetsController?.hide(WindowInsets.Type.statusBars())
@@ -25,12 +25,18 @@ class IntroActivity : AppCompatActivity() {
 
         }
 
-        binding.btnSignUpIntro.setOnClickListener {
-            startActivity(Intent(this,SignUpActivity::class.java))
+
+    }
+
+    private fun setupActionBar(){
+
+        setSupportActionBar(binding.toolbarSignUpActivity)
+        val actionBar = supportActionBar
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
         }
 
-
-
-
+        binding.toolbarSignUpActivity.setNavigationOnClickListener { onBackPressed() }
     }
 }
